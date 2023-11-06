cc72854eb3a706e9a16ae0acf7b4002474454f9c
JoaoVFBraga <143449194+JoaoVFBraga@users.noreply.github.com>
2023-11-06 19:20:43 -0300

    Add files via upload

---
+++ a37-javascript/script.js
@@ -0,0 +1,8 @@
+console.log('Olá, mundo!');
+const nome = 'João Victor';
+console.log(nome);
+console.log('As váriaveis definidas como const não podem ser reatribuidas.');
+let sobrenome = 'Santana';
+sobrenome = 'Furlan Braga';
+console.log(nome, sobrenome);
+console.log('As váriaveis definidas como let podem ser redefinidas.');
---
+++ a38-manipularDOM/script.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+// Salva o elemento HTML escrito entre aspas numa váriavel
+console.log(produtos.innerHTML);
+// Imprime no console apenas o conteudo da várivael produtos
+nav.style.backgroundColor = "yellow";
+// Podemos fazer alterações no código inteiro com JS.
