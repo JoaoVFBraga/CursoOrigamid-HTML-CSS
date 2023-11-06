4ac3691ce709cf16388d32f2e9147af9c89e3134
JoaoVFBraga <143449194+JoaoVFBraga@users.noreply.github.com>
2023-11-06 19:21:52 -0300

    Add files via upload

---
+++ a39-tiposdedados/script.js
@@ -0,0 +1,13 @@
+// String
+const nome = 'João Victor';
+const sobrenome = 'Furlan Braga';
+const nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+// Number
+let lucroLiquido = 0;
+
+const lucro = 30;
+const imposto = 10;
+lucroLiquido = lucro - imposto;
+console.log(lucroLiquido);
